37d5efeb9f31e18872335e42aac651e7ed356d83
Sebastian Salamanca <sebastiansalamanca@aol.com>
2018-02-07 14:58:56 -0500

    inti commit

---
+++ GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  goadpost-app
+//
+//  Created by Sebastian Salamanca on 2/7/18.
+//  Copyright © 2018 Sebastian Salamanca. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GoalType.swift
@@ -0,0 +1,9 @@
+//
+//  GoalType.swift
+//  goadpost-app
+//
+//  Created by Sebastian Salamanca on 2/7/18.
+//  Copyright © 2018 Sebastian Salamanca. All rights reserved.
+//
+
+import Foundation
